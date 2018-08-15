529062f3dc2a230c0fe55442534c9cf34ce24bff
IshanArora21 <34289247+IshanArora21@users.noreply.github.com>
2018-08-15 15:59:48 -0400

    use securer password_hash (default 60 chars)

--- server.php
+++ server.php
@@ -44,7 +44,7 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+  	$password = password_hash($password_1, PASSWORD_DEFAULT);//encrypt the password before saving in the database
 
   	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
@@ -71,7 +71,7 @@ if (isset($_POST['login_user'])) {
     }
   
     if (count($errors) == 0) {
-        $password = md5($password);
+        $password = password_hash($password, PASSWORD_DEFAULT);
         $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
