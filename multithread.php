a1fc19f5ab0749db6329fdd811d1db2e26b2a0e0
IshanArora21 <34289247+IshanArora21@users.noreply.github.com>
2019-04-27 22:16:20 -0400

    implement basic class framework

--- multithread.php
+++ multithread.php
@@ -1,3 +1,23 @@
-<?php 
-session_start();
-?>
+<?php
+class MultiThread extends Thread {
+  private $threadId;
+  public $data;
+  public function __construct(int $id) {
+    $this->threadId = $id;
+    $this->data = $id . ":" . date('H:i:s');
+  }
+  public function run() {
+  
+    //thread started
+    echo 'thread ' . $this->threadId . "  started.\n";
+    
+    //sleep for <threadId> seconds
+    sleep($this->threadId)
+    
+    //thread ended with timestamp
+    echo 'thread ' . $this->threadId . " ended at " . date('H:i:s') . "\n"
+  }
+}
+ 
+
+} 
